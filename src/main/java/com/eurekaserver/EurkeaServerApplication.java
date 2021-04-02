c5d71b9195c38cb5e2977cc9116123f493c08cc8
Valeria Meza <valeriameza173@gmail.com>
2021-04-02 01:00:02 -0500

    add congiguration to application.yml for eureka server

---
+++ src/main/java/com/eurekaserver/EurkeaServerApplication.java
@@ -0,0 +1,15 @@
+package com.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurkeaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurkeaServerApplication.class, args);
+	}
+
+}
